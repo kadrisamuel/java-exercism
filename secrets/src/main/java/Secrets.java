ab61bd33baf296006b3aead75378bf48c07fff02
Kadri Samuel <kadrisamuel@gmail.com>
2025-01-11 16:31:38 +0100

    Pulled down the exercise and added main method

---
+++ secrets/src/main/java/Secrets.java
@@ -0,0 +1,20 @@
+public class Secrets {
+    public static void main(String[] args) {
+        
+    }
+    public static int shiftBack(int value, int amount) {
+        throw new UnsupportedOperationException("Please implement the (static) Secrets.shiftBack() method");
+    }
+
+    public static int setBits(int value, int mask) {
+        throw new UnsupportedOperationException("Please implement the (static) Secrets.setBits() method");
+    }
+
+    public static int flipBits(int value, int mask) {
+        throw new UnsupportedOperationException("Please implement the (static) Secrets.flipBits() method");
+    }
+
+    public static int clearBits(int value, int mask) {
+        throw new UnsupportedOperationException("Please implement the (static) Secrets.clearBits() method");
+    }
+}
