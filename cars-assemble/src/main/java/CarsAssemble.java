5af261584b403faa608a0d909a82ee4ff6438033
Kadri Samuel <kadrisamuel@gmail.com>
2025-01-11 17:47:36 +0100

    Added code review changes to cars-asseemble exercise

--- cars-assemble/src/main/java/CarsAssemble.java
+++ cars-assemble/src/main/java/CarsAssemble.java
@@ -1,33 +1,30 @@
 public class CarsAssemble {
+    private static final int DEFAULT_PRODUCTION_RATE = 221;
     public static void main (String[] args) {
         CarsAssemble line = new CarsAssemble();
         double rate = line.productionRatePerHour(7);
         System.out.println(rate);
     }
-    public double productionRatePerHour(int speed) {
-        int grossRate = speed * 221;
-        double netRate = 0;
-        if (speed > 0) {
-            if (speed >= 1 && speed <= 4 ) {
-                return grossRate;
-            } else if (speed >= 5 && speed <= 8) {
-                return netRate = grossRate * 0.9;
-            } else if (speed == 9){
-                return netRate = grossRate * 0.8;
-            } else if (speed == 10) {
-                return netRate = grossRate * 0.77;
-            } else {
-                throw new UnsupportedOperationException("Input should between 0 and 10");
-            }
-        }
 
-        return netRate;
+    private double getSuccessRate (int speed) {
+        if (speed == 0){
+            return 0;
+        } else if (speed >= 1 && speed <= 4 ) {
+            return 1;
+        } else if (speed >= 5 && speed <= 8) {
+            return 0.9;
+        } else if (speed == 9){
+            return 0.8;
+        } else { //(speed == 10)
+            return 0.77;
+        }
     }
 
+    public double productionRatePerHour(int speed) {
+        return speed * DEFAULT_PRODUCTION_RATE * getSuccessRate(speed);
+    }
     public int workingItemsPerMinute(int speed) {
-        double carsPerHour = productionRatePerHour(speed);
-        int carsPerMinute = (int) (carsPerHour / 60);
-        return carsPerMinute;
+        return (int) (productionRatePerHour(speed)/60);
     }
 }
 
