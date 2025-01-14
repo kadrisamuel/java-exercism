6533aea4232472cb1c6a935e4ca8b819b9eec08a
Kadri Samuel <kadrisamuel@gmail.com>
2025-01-14 15:22:25 +0100

    Improved solution: from switch statements to switch expressions, new kind of case label from colon to comma, clarified error string

--- football-match-reports/src/main/java/FootballMatchReports.java
+++ football-match-reports/src/main/java/FootballMatchReports.java
@@ -1,58 +1,19 @@
-public class FootballMatchReports {    
+public class FootballMatchReports {   
+    public static void main(String[] args) {
+        String sth = onField(4);
+        System.out.println(sth);
+    }
     public static String onField(int shirtNum) {
-        String description = "";
-        switch (shirtNum) {
-            case 1:
-                description = "goalie";
-                break;
-            case 2:
-                description = "left back";
-                break;
-            case 3: case 4:
-                description = "center back";
-                break;
-            case 5:
-                description = "right back";
-                break;
-            case 6: case 7: case 8:
-                description = "midfielder";
-                break;
-            case 9: 
-                description = "left wing";
-                break;
-            case 10: 
-                description = "striker";
-                break;
-            case 11:
-                description = "right wing";
-                break;
-            default:
-                throw new IllegalArgumentException("Shirt number outside the range 1-11 is processed in FootballMatchReports.onField() method");
-        }
-        return description;
+        return switch (shirtNum) {
+            case 1 ->       "goalie";
+            case 2 ->       "left back";
+            case 3, 4 ->    "center back";
+            case 5 ->       "right back";
+            case 6, 7, 8 -> "midfielder";
+            case 9 ->       "left wing";
+            case 10 ->      "striker";
+            case 11 ->      "right wing";
+            default -> throw new IllegalArgumentException("Shirt number outside the range 1-11 in FootballMatchReports.onField() method");
+        };
     }
 }
-
-
-/* String direction = getDirection();
-switch (direction) {
-    case "left":
-        goLeft();
-        break;
-    case "right":
-        goRight();
-        break;
-    default:
-        // otherwise
-        markTime();
-        break;
-}
-
-1 -> "goalie"
-2 -> "left back"
-3 & 4 "center back"
-5 -> "right back"
-6, 7, & 8 -> "midfielder"
-9 -> "left wing"
-10 -> "striker"
-11 -> "right wing" */
