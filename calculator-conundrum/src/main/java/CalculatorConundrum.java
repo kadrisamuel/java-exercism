6d546d103462bb13e2a4d8a2c55f1e5be1e2f0e7
Kadri Samuel <kadrisamuel@gmail.com>
2025-01-20 22:16:41 +0100

    Solved exceptions exercise, it could be improved by using a switch based on operations and separating throwing exceptions in methods for arithmetic operations for better readability

--- calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,5 +1,24 @@
 class CalculatorConundrum {
-    public String calculate(int operand1, int operand2, String operation) {
-        throw new UnsupportedOperationException("Please implement the CalculatorConundrum.calculate() method");
+    public String calculate(int operand1, int operand2, String operation) throws IllegalOperationException {
+        int result;
+        if (operation == "+") {
+            result = operand1 + operand2;
+        } else if (operation == "*") {
+            result = operand1 * operand2;
+        } else if (operation == "/") {
+            try {
+                result = operand1 / operand2;
+            } catch (ArithmeticException e) {
+                throw new IllegalOperationException("Division by zero is not allowed", e);
+            } 
+        } else {
+            if (operation == null) {
+                throw new IllegalArgumentException("Operation cannot be null");
+            } else if (operation == "") {
+                throw new IllegalArgumentException("Operation cannot be empty");
+            }
+            throw new IllegalOperationException(String.format("Operation '%s' does not exist", operation));
+        }
+        return String.format("%d %s %d = %d", operand1, operation, operand2, result);
     }
 }
