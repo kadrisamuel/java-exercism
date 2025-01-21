616db9011d92a07473ebf16032f7193e9d8abb00
Kadri Samuel <kadrisamuel@gmail.com>
2025-01-21 16:28:29 +0100

    Improved solution

--- gotta-snatch-em-all/src/main/java/GottaSnatchEmAll.java
+++ gotta-snatch-em-all/src/main/java/GottaSnatchEmAll.java
@@ -1,15 +1,11 @@
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
 class GottaSnatchEmAll {
 
     static Set<String> newCollection(List<String> cards) {
-        Set<String> newCollection = new HashSet<>();
-        for (int i = 0; i < cards.size(); i++) {
-            newCollection.add(cards.get(i));
-        }
+        Set<String> newCollection = new HashSet<>(cards);
         return newCollection;
     }
 
@@ -23,36 +19,14 @@ class GottaSnatchEmAll {
 
     static Set<String> commonCards(List<Set<String>> collections) {
         Set<String> newCollection = new HashSet<>();
-        Iterator<String> cardsIterator = collections.get(0).iterator();
-
-        while (cardsIterator.hasNext() ) {
-            String card = cardsIterator.next();
-            int setsWithCard = 0;
-
-            for (int i = 0; i < collections.size(); i++) {
-                if (collections.get(i).contains(card) && setsWithCard < collections.size()) {
-                    setsWithCard += 1;
-                }
-                if (setsWithCard == collections.size()) {
-                    newCollection.add(card);
-                }
-            }
-        }
-
+        newCollection.addAll(collections.get(0));
+        collections.forEach(newCollection::retainAll);
         return newCollection;
-        
     }
 
     static Set<String> allCards(List<Set<String>> collections) {
         Set<String> allCards = new HashSet<>();
-
-        for (int i = 0; i < collections.size(); i++) {
-            Iterator<String> cardsIterator = collections.get(i).iterator();
-            while (cardsIterator.hasNext()) {
-                allCards.add(cardsIterator.next());
-            }
-        }
-
+        collections.forEach(allCards::addAll);
         return allCards;
     }
 }
