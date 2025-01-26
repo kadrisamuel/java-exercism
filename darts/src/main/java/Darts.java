4de892c17150a42687a4911350fd06763c85d61a
Kadri Samuel <kadrisamuel@gmail.com>
2025-01-26 23:56:04 +0100

    my solution

--- darts/src/main/java/Darts.java
+++ darts/src/main/java/Darts.java
@@ -1,5 +1,21 @@
 class Darts {
     int score(double xOfDart, double yOfDart) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        final double INNER_CIRCLE = 1;
+        final double MIDDLE_CIRCLE = 5 * 5;
+        final double OUTER_CIRCLE = 10 * 10;
+
+        double x2PlusY2 = Math.pow(Math.abs(xOfDart), 2) + Math.pow(Math.abs(yOfDart), 2);
+
+        if (x2PlusY2 <= OUTER_CIRCLE) {
+            if (x2PlusY2 <= MIDDLE_CIRCLE) {
+                if (x2PlusY2 <= INNER_CIRCLE) {
+                    return 10;
+                }
+                return 5;
+            }
+            return 1;
+        }
+
+        return 0;
     }
 }
