6ce638b363b2597d8081225359e4278cab25c496
Kadri Samuel <kadrisamuel@gmail.com>
2025-01-28 09:27:03 +0200

    Improved solution based on bobahop's solution but didn't use predicate

--- darts/src/main/java/Darts.java
+++ darts/src/main/java/Darts.java
@@ -1,23 +1,23 @@
-// Better solution to think about https://exercism.org/tracks/java/exercises/darts/solutions/bobahop 
+// Predicate solution: https://exercism.org/tracks/java/exercises/darts/solutions/bobahop 
 class Darts {
     final private static double INNER_CIRCLE = 1.0;
     final private static double MIDDLE_CIRCLE = 5.0;
     final private static double OUTER_CIRCLE = 10.0;
 
-    int score(double xOfDart, double yOfDart) {
-
-        double radius = Math.hypot(xOfDart, yOfDart);
-
+    int calculate(double radius) {
+        if (radius <= INNER_CIRCLE) {
+            return 10;
+        }
+        if (radius <= MIDDLE_CIRCLE) {
+            return 5;
+        }
         if (radius <= OUTER_CIRCLE) {
-            if (radius <= MIDDLE_CIRCLE) {
-                if (radius <= INNER_CIRCLE) {
-                    return 10;
-                }
-                return 5;
-            }
             return 1;
         }
-
         return 0;
+    }
+
+    int score(double xOfDart, double yOfDart) {
+        return calculate(Math.hypot(xOfDart, yOfDart));
     }
 }
