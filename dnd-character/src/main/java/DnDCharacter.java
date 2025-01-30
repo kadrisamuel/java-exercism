a20350da4cdaf53ed7ede8ec34f2881d1789ed02
Kadri Samuel <kadrisamuel@gmail.com>
2025-01-30 17:12:43 +0200

    Improved solution

--- dnd-character/src/main/java/DnDCharacter.java
+++ dnd-character/src/main/java/DnDCharacter.java
@@ -4,29 +4,30 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 class DnDCharacter {
-    private int strength;
-    private int dexterity;
-    private int constitution;
-    private int intelligence;
-    private int wisdom;
-    private int charisma;
-    private int hitpoints;
-    private boolean strengthSet;
-    private boolean dexteritySet;
-    private boolean constitutionSet;
-    private boolean intelligenceSet;
-    private boolean wisdomSet;
-    private boolean charismaSet;
-    private boolean hitpointsSet;
+    private final int strength;
+    private final int dexterity;
+    private final int constitution;
+    private final int intelligence;
+    private final int wisdom;
+    private final int charisma;
+    private final int hitpoints;
 
     ThreadLocalRandom random = ThreadLocalRandom.current();
 
+    public DnDCharacter () {
+        this.strength = getAbility();
+        this.dexterity = getAbility();
+        this.constitution = getAbility();
+        this.intelligence = getAbility();
+        this.wisdom = getAbility();
+        this.charisma = getAbility();
+        this.hitpoints = 10 + modifier(this.constitution);
+    }
+
     int ability(List<Integer> scores) {
-        List<Integer> mutableList = new ArrayList<>(scores);
-        mutableList.remove(mutableList.indexOf(Collections.min(mutableList)));
-        int sum = 0;
-        for (int i = 0; i < mutableList.size(); i++) {
-            sum += mutableList.get(i);
+        int sum = 0 - Collections.min(scores);
+        for (int i = 0; i < scores.size(); i++) {
+            sum += scores.get(i);
         }
         return sum;
     }
@@ -40,62 +41,38 @@ class DnDCharacter {
     }
 
     int modifier(int input) {
-        return (int) Math.floor(((double)(input - 10))/2);
+        return Math.floorDiv(input - 10, 2);
+    }
+
+    int getAbility () {
+        return ability(rollDice());
     }
 
     int getStrength() {
-        if (!strengthSet) {
-            this.strength = ability(rollDice());
-            strengthSet = true;
-        }
         return strength;
     }
 
     int getDexterity() {
-        if (!dexteritySet) {
-            this.dexterity = ability(rollDice());
-            dexteritySet = true;
-        }
         return dexterity;
     }
 
     int getConstitution() {
-        if (!constitutionSet) {
-            this.constitution = ability(rollDice());
-            constitutionSet = true;
-        }
         return constitution;
     }
 
     int getIntelligence() {
-        if (!intelligenceSet) {
-            this.intelligence = ability(rollDice());
-            intelligenceSet = true;
-        }
         return intelligence;
     }
 
     int getWisdom() {
-        if (!wisdomSet) {
-            this.wisdom = ability(rollDice());
-            wisdomSet = true;
-        }
         return wisdom;
     }
 
     int getCharisma() {
-        if (!charismaSet) {
-            this.charisma = ability(rollDice());
-            charismaSet = true;
-        }
         return charisma;
     }
 
     int getHitpoints() {
-        if (!hitpointsSet) {
-            this.hitpoints = 10 + modifier(this.constitution);
-            hitpointsSet = true;
-        }
         return hitpoints;
     }
 }
