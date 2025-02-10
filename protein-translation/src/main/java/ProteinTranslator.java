ab22b3d54ecc984cd3ce7177009cc54eab072936
Kadri Samuel <kadrisamuel@gmail.com>
2025-02-10 10:24:29 +0100

    All tests pass

--- protein-translation/src/main/java/ProteinTranslator.java
+++ protein-translation/src/main/java/ProteinTranslator.java
@@ -1,8 +1,49 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 class ProteinTranslator {
+    private ArrayList<String> proteins = new ArrayList<String>();
+    private String protein;
 
-    List<String> translate(String rnaSequence) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    HashMap<String, String> codonProtein = new HashMap<>(){{
+        put("AUG", "Methionine");
+        put("UUU", "Phenylalanine");
+        put("UUC", "Phenylalanine");
+        put("UUA", "Leucine");
+        put("UUG", "Leucine");
+        put("UCU", "Serine");
+        put("UCC", "Serine");
+        put("UCA", "Serine");
+        put("UCG", "Serine");
+        put("UAU", "Tyrosine");
+        put("UAC", "Tyrosine");
+        put("UGU", "Cysteine");
+        put("UGC", "Cysteine");
+        put("UGG", "Tryptophan");
+        put("UAA", "STOP");
+        put("UAG", "STOP");
+        put("UGA", "STOP");
+    }};
+
+    List<String> translate(String rnaSequence) throws IllegalArgumentException {
+        for (int index = 0; index < rnaSequence.length(); index += 3) {
+            
+            try {
+                protein = codonProtein.get(rnaSequence.substring(index, index+3));
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Invalid codon");
+            }
+
+            if (protein == null) {
+                throw new IllegalArgumentException("Invalid codon");
+            }
+            if (protein == "STOP") {
+                return proteins;
+            }
+
+            proteins.add(protein);
+        }
+        return proteins;
     }
 }
