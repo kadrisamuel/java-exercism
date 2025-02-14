b84bac25dfb88b631cc4e1fa3278ecc3fe68cd28
Kadri Samuel <kadrisamuel@gmail.com>
2025-02-14 20:39:11 +0100

    Replaced for loop with intstream

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -1,24 +1,20 @@
+import java.util.stream.IntStream;
+
 public class Hamming {
-    public String left;
-    public String right;
-    public int length;
-    private int distance;
+
+    final private int distance;
 
     public Hamming(String leftStrand, String rightStrand) throws IllegalArgumentException {
-        
+
         if (leftStrand.length() != rightStrand.length()) {
             throw new IllegalArgumentException("strands must be of equal length");
         }
 
-        this.length = leftStrand.length();
-        this.left = leftStrand;
-        this.right = rightStrand;
+        this.distance = (int) IntStream
+        .range(0, leftStrand.length())
+        .filter(i -> leftStrand.charAt(i) != rightStrand.charAt(i))
+        .count();
 
-        for (int i = 0; i < length; i++) {
-            if (left.charAt(i) != right.charAt(i)) {
-                this.distance += 1;
-            }
-        }
     }
 
     public int getHammingDistance() {
