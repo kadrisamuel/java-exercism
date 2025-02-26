9b6e88464d5b1f2ecf56e4f8b1fbe6ed6f95d310
Kadri Samuel <kadrisamuel@gmail.com>
2025-02-26 16:24:13 +0100

    All tests pass

--- international-calling-connoisseur/src/main/java/DialingCodes.java
+++ international-calling-connoisseur/src/main/java/DialingCodes.java
@@ -1,34 +1,43 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class DialingCodes {
+    private Map<Integer, String> codes = new HashMap<>();
 
     public Map<Integer, String> getCodes() {
-        throw new UnsupportedOperationException(
-                "Delete this statement and write your own implementation.");
+        return this.codes;
     }
 
     public void setDialingCode(Integer code, String country) {
-        throw new UnsupportedOperationException(
-                "Delete this statement and write your own implementation.");
+        this.codes.put(code, country);
     }
 
     public String getCountry(Integer code) {
-        throw new UnsupportedOperationException(
-                "Delete this statement and write your own implementation.");
+        return codes.get(code);
     }
 
     public void addNewDialingCode(Integer code, String country) {
-        throw new UnsupportedOperationException(
-                "Delete this statement and write your own implementation.");
+        if (!codes.containsKey(code) && !codes.containsValue(country)) {
+            codes.put(code, country);
+        }
     }
 
     public Integer findDialingCode(String country) {
-        throw new UnsupportedOperationException(
-                "Delete this statement and write your own implementation.");
+        if (!codes.containsValue(country)) {
+            return null;
+        }
+        return codes
+            .entrySet()
+            .stream()
+            .filter(entry -> country.equals(entry.getValue()))
+            .mapToInt(Map.Entry::getKey)
+            .toArray()[0];
     }
 
     public void updateCountryDialingCode(Integer code, String country) {
-        throw new UnsupportedOperationException(
-                "Delete this statement and write your own implementation.");
+        if (codes.containsValue(country)) {
+            codes.remove(findDialingCode(country));
+            setDialingCode(code, country);
+        }
     }
 }
