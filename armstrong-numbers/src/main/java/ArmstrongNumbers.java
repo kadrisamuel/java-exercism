7f28de1258639be12e3beaec8cf8683e21009afc
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-01 14:00:13 +0100

    implemented input length determination

--- armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,15 @@
 class ArmstrongNumbers {
+    static int length;
 
     boolean isArmstrongNumber(int numberToCheck) {
-
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-
+        if (numberToCheck != 0) {
+            numberToCheck = Math.abs(numberToCheck);
+            length = (int) (Math.log10(numberToCheck) + 1);
+            for (int i = 1; i < length + 1; i++) {
+                
+            }
+        }
+        return true;
     }
 
 }
