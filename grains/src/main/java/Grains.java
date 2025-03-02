014700a9da93ad7bdfdfd707e85915002ebf82d9
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-02 17:01:49 +0100

    Added functionality to calculate a given square

--- grains/src/main/java/Grains.java
+++ grains/src/main/java/Grains.java
@@ -1,9 +1,13 @@
 import java.math.BigInteger;
 
-class Grains {
 
-    BigInteger grainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+class Grains {
+    // formula: 2^(x-1)
+    BigInteger grainsOnSquare(final int square) throws IllegalArgumentException {
+        if (square <= 0 || square > 64) {
+            throw new IllegalArgumentException("square must be between 1 and 64");
+        }
+        return BigInteger.TWO.pow(square-1);
     }
 
     BigInteger grainsOnBoard() {
