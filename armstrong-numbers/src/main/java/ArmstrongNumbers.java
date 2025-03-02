a27159839f0288cf9a1136577afb634cb1832790
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-02 17:55:31 +0100

    added solution option with reduce instead of map and sum

--- armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -6,7 +6,9 @@ class ArmstrongNumbers {
         String asStr = Integer.toString(numberToCheck);
         int length = asStr.length();
         
-        return numberToCheck == asStr.chars().map(c -> (int) Math.pow(Character.getNumericValue(c), length)).sum();
+        //return numberToCheck == asStr.chars().map(c -> (int) Math.pow(Character.getNumericValue(c), length)).sum();
+        return numberToCheck == asStr.chars().reduce(0, (subtotal, c) -> subtotal + (int) Math.pow(Character.getNumericValue(c), length));
+
     }
 
 }
