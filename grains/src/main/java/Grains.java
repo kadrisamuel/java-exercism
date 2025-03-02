58bea6a0a71e53ece54764497778da3e3ca648d7
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-02 19:48:22 +0100

    Final solution

--- grains/src/main/java/Grains.java
+++ grains/src/main/java/Grains.java
@@ -1,22 +1,16 @@
 import java.math.BigInteger;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 
 class Grains {
+    
     // formula: 2^(x-1)
     BigInteger grainsOnSquare(final int square) throws IllegalArgumentException {
-        if (square <= 0 || square > 64) {
+        if (square < 1 || square > 64) {
             throw new IllegalArgumentException("square must be between 1 and 64");
         }
         return BigInteger.TWO.pow(square-1);
     }
 
     BigInteger grainsOnBoard() {
-        BigInteger total = BigInteger.ZERO;
-        for (int i = 1; i < 65; i++) {
-            total = total.add(grainsOnSquare(i));
-        }
-        return total;
-    }    
+        return BigInteger.TWO.pow(64).subtract(BigInteger.ONE);
+    }
 }
