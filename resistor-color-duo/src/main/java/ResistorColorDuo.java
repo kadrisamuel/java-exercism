0030d5e76c9ddafdc62d8f4e90ad55fcb69db528
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-03 15:13:25 +0100

    added applicable parts from previous exercise

--- resistor-color-duo/src/main/java/ResistorColorDuo.java
+++ resistor-color-duo/src/main/java/ResistorColorDuo.java
@@ -1,4 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
 class ResistorColorDuo {
+    private static final String[] colors = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+    private static final List<String> lookup = Arrays.asList(colors);
+    
+    int colorCode(String color) {
+        return lookup.indexOf(color);
+    }
+
     int value(String[] colors) {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
