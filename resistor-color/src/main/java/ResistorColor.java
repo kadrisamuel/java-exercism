c1aff4bc0781c69fee4a516b21a6ab335442e257
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-03 14:59:36 +0100

    Initial solution

--- resistor-color/src/main/java/ResistorColor.java
+++ resistor-color/src/main/java/ResistorColor.java
@@ -1,9 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
 class ResistorColor {
+    private static final String[] colors = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+    private static final Map<String, Integer> lookup = new HashMap<>();
+    static {
+        IntStream.range(0, colors.length).forEach(i -> lookup.put(colors[i], i));
+    }
+    
     int colorCode(String color) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return lookup.get(color);
     }
 
     String[] colors() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return colors;
     }
 }
