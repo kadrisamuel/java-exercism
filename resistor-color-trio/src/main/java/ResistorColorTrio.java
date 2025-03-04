5c1dde65dd93fed795b5a54adcff0c7dc2cd2c32
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-04 12:00:01 +0100

    Cleaned up solution

--- resistor-color-trio/src/main/java/ResistorColorTrio.java
+++ resistor-color-trio/src/main/java/ResistorColorTrio.java
@@ -5,34 +5,12 @@ class ResistorColorTrio {
     private static final String[] colors = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
     private static final List<String> lookup = Arrays.asList(colors);
 
-    int stripTail(int originalValue, int toStrip){
-        return Integer.parseInt(String.valueOf(originalValue).substring(0, String.valueOf(originalValue).length() - toStrip));
-    }
-
-    boolean trailingZero(int input){
-        String value = String.valueOf(input);
-        return value.charAt(value.length() - 1) == '0';
-    }
-
     String label(String[] colors) {
         int value = (lookup.indexOf(colors[0]) * 10 + lookup.indexOf(colors[1]));
-        boolean extraZero = trailingZero(value);
         int exponent = lookup.indexOf(colors[2]);
-
-
-        if (exponent < 2) {
-            value = value * (int) Math.pow(10, exponent);
-        } else {
-            if (exponent % 3 != 0 && extraZero) {      
-            value = stripTail(value, 1);
-            exponent += 1;
-            }
-            if (exponent % 3 != 0 && !extraZero){
-                value = value * 10;
-                exponent -= 1;
-            }
-        }
-
+        List<Integer> resistance = equalize(value, exponent);
+        value = resistance.get(0);
+        exponent = resistance.get(1);
 
         switch (exponent) {
             case 0, 1, 2:
@@ -52,6 +30,32 @@ class ResistorColorTrio {
         }
     }
 
+    int stripTail(int originalValue, int toStrip){
+        return Integer.parseInt(String.valueOf(originalValue).substring(0, String.valueOf(originalValue).length() - toStrip));
+    }
+
+    boolean trailingZero(int input){
+        String value = String.valueOf(input);
+        return value.charAt(value.length() - 1) == '0';
+    }
+
+    List<Integer> equalize(int value, int exponent) {
+        boolean extraZero = trailingZero(value);
+        if (exponent < 2) {
+            value = value * (int) Math.pow(10, exponent);
+        } else {
+            if (exponent % 3 != 0 && extraZero) {      
+            value = stripTail(value, 1);
+            exponent += 1;
+            }
+            if (exponent % 3 != 0 && !extraZero){
+                value = value * 10;
+                exponent -= 1;
+            }
+        }
+        return List.of(value, exponent);
+    }
+    
 }
 
 
