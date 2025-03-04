14bdf9c1e47015c9b0137728d7cd97cff2a5beb9
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-04 16:10:13 +0100

    Initial solution

--- rna-transcription/src/main/java/RnaTranscription.java
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class RnaTranscription {
+    final static private Character[] _dna = {'G', 'C', 'T', 'A'};
+    final static private Character[] _rna = {'C', 'G', 'A', 'U'};
+    final static private List<Character> DNA = Arrays.asList(_dna);
+    final static private List<Character> RNA = Arrays.asList(_rna);
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return dnaStrand.chars()
+            .mapToObj(c ->  RNA.get(DNA.indexOf((char)c)).toString())
+            .collect(Collectors.joining());         
     }
 
 }
