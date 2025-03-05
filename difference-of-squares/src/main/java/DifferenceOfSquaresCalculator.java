36935fb651480e98f49dea1c5bf6f7e2684521c0
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-05 21:53:23 +0100

    Initial solution

--- difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,15 +1,18 @@
+import java.util.stream.IntStream;
+
 class DifferenceOfSquaresCalculator {
 
     int computeSquareOfSumTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return (int) Math.pow(IntStream.rangeClosed(1, input).sum(), 2);
     }
 
+    // [n(n+1)(2n+1)] / 6
     int computeSumOfSquaresTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return (input*(input + 1)*(2 * input + 1))/6;
     }
 
     int computeDifferenceOfSquares(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return Math.abs(computeSumOfSquaresTo(input) - computeSquareOfSumTo(input));
     }
 
 }
