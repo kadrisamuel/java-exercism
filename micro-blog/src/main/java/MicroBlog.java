2aebc6c068aad0d02843fa759fba602da6d06a89
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-06 12:54:18 +0100

    Improved solution uses StringBuilder with appendCodePoint(), better for performance and memory usage

--- micro-blog/src/main/java/MicroBlog.java
+++ micro-blog/src/main/java/MicroBlog.java
@@ -1,10 +1,11 @@
-import java.util.stream.Collectors;
 
 class MicroBlog {
+    private final static int MAX_POST_LENGTH = 5;
+
     public String truncate(String input) {
         return input.codePoints()
-            .limit(5)
-            .mapToObj(c -> Character.toString(c))
-            .collect(Collectors.joining());
+            .limit(MAX_POST_LENGTH)
+            .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+            .toString();
     }
 }
