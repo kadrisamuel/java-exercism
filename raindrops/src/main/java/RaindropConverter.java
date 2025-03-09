48f587f86e9e02553fefad655a14b10049042580
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-09 11:43:36 +0100

    Initial solution

--- raindrops/src/main/java/RaindropConverter.java
+++ raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,23 @@
 class RaindropConverter {
+    final static String divBy3 = "Pling";
+    final static String divBy5 = "Plang";
+    final static String divBy7 = "Plong";
 
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder result = new StringBuilder();
+        if (number % 3 == 0) {
+            result.append(divBy3);
+        }
+        if (number % 5 == 0) {
+            result.append(divBy5);
+        }
+        if (number % 7 == 0) {
+            result.append(divBy7);
+        }
+        if (result.isEmpty()) {
+            result.append(String.valueOf(number));
+        }
+        return result.toString();
     }
 
 }
