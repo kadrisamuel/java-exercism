659b423405ced1dc08c85ff41752ef2cf4f1f39a
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-09 15:18:26 +0100

    Solution with map and stream

--- raindrops/src/main/java/RaindropConverter.java
+++ raindrops/src/main/java/RaindropConverter.java
@@ -1,23 +1,20 @@
+import java.util.HashMap;
+
 class RaindropConverter {
-    final static String divBy3 = "Pling";
-    final static String divBy5 = "Plang";
-    final static String divBy7 = "Plong";
+    final static HashMap<Integer, String> lookup = new HashMap<>(){{
+        put(3, "Pling");
+        put(5, "Plang");
+        put(7, "Plong");
+    }};
+
 
     String convert(int number) {
-        StringBuilder result = new StringBuilder();
-        if (number % 3 == 0) {
-            result.append(divBy3);
-        }
-        if (number % 5 == 0) {
-            result.append(divBy5);
-        }
-        if (number % 7 == 0) {
-            result.append(divBy7);
-        }
-        if (result.isEmpty()) {
-            result.append(String.valueOf(number));
-        }
-        return result.toString();
+        return lookup.keySet()
+            .stream()
+            .filter(key -> number % key == 0)
+            .map(key -> lookup.get(key))
+            .reduce((s1, s2) -> s1 + s2)
+            .orElse(Integer.toString(number));
     }
 
 }
