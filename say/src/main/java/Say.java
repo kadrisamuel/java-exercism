beec74262f4781adeff79268e2c917d4de8243d4
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-10 20:56:07 +0100

    Added numbers to map and exception handling

--- say/src/main/java/Say.java
+++ say/src/main/java/Say.java
@@ -1,6 +1,46 @@
+import java.util.HashMap;
+
 public class Say {
+    HashMap<Integer, String> lookup = new HashMap<>(){{
+        put(-1, "-");
+        put(0, "zero");
+        put(1, "one");
+        put(2, "two");
+        put(3, "three");
+        put(4, "four");
+        put(5, "five");
+        put(6, "six");
+        put(7, "seven");
+        put(8, "eight");
+        put(9, "nine");
+        put(10, "ten");
+        put(11, "eleven");
+        put(12, "twelve");
+        put(13, "thirteen");
+        put(14, "fourteen");
+        put(15, "fifteen");
+        put(16, "sixteen");
+        put(17, "seventeen");
+        put(18, "eighteen");
+        put(19, "nineteen");
+        put(20, "twenty");
+        put(30, "thirty");
+        put(40, "forty");
+        put(50, "fifty");
+        put(60, "sixty");
+        put(70, "seventy");
+        put(80, "eighty");
+        put(90, "ninety");
+        put(100, "hundred");
+        put(103, "thousand");
+        put(106, "million");
+        put(109, "billion");
+    }};
 
     public String say(long number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (number < 0 || number > 999_999_999_999L) {
+            throw new IllegalArgumentException("Input has to be between 0 and 999,999,999,999");
+        }
+
     }
 }
