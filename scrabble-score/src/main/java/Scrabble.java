b08fd930497bfba06d0af9638507fd70391b804f
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-14 14:22:34 +0100

    Initial solution

--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -1,11 +1,28 @@
 class Scrabble {
+    private int score;
 
     Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.score = word.codePoints().map(letter -> getPoints((char) letter)).sum();
     }
 
+
     int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.score;
+    }
+
+    private int getPoints(char letter) {
+        letter = Character.toUpperCase(letter);
+
+        return switch (letter) {
+            case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' -> 1;        
+            case 'D', 'G' -> 2;
+            case 'B', 'C', 'M', 'P' -> 3;
+            case 'F', 'H', 'V', 'W', 'Y' -> 4;
+            case 'K' -> 5;
+            case 'J', 'X' -> 8;
+            case 'Q', 'Z' -> 10;
+            default -> 0;
+        };
     }
 
 }
