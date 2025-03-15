9b2acb97537b9ba526f3856cb6a4202964e352be
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-15 19:21:20 +0100

    Initial solution

--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 class HandshakeCalculator {
+    private List<Signal> output;
+    private int maxDigits;
 
     List<Signal> calculateHandshake(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        output  = new ArrayList<>(Integer.bitCount(number));
+        String input = new StringBuffer(Integer.toBinaryString(number))
+            .reverse().toString();
+        maxDigits = (input.length() < 4)? input.length() : 4;
+
+        IntStream.range(0, maxDigits)
+            .filter(x -> input.charAt(x) == '1')
+            .forEach(x -> output.add(Signal.getSignal(x)));
+
+
+        if (input.length() > 4 && input.charAt(4) == '1') {
+            return output.reversed();
+        }
+
+        return output;
     }
 
 }
--- secret-handshake/src/main/java/Signal.java
+++ secret-handshake/src/main/java/Signal.java
@@ -1,5 +1,20 @@
 enum Signal {
 
-    WINK, DOUBLE_BLINK, CLOSE_YOUR_EYES, JUMP
+    WINK(0), DOUBLE_BLINK(1), CLOSE_YOUR_EYES(2), JUMP(3);
+
+    private final int number;
+    
+    Signal(int number) {
+        this.number = number;
+    }
+
+    public static Signal getSignal(int number){
+        for (Signal signal : Signal.values()) {
+            if (signal.number == number) {
+                return signal;
+            }
+        }
+        throw new IllegalArgumentException("No signal with number " + number);
+    }
 
 }
