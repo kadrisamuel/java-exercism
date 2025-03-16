27cc3188da8b89f7f08d06d5c7f6e022fde5e667
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-16 13:24:43 +0100

    Some beautification

--- space-age/src/main/java/SpaceAge.java
+++ space-age/src/main/java/SpaceAge.java
@@ -1,46 +1,21 @@
 class SpaceAge {
     private final double seconds;
     private final double earthAge;
+    private final static double EARTH_SECONDS = 31_557_600;
 
     SpaceAge(double seconds) {
         this.seconds = seconds;
-        this.earthAge = seconds / 31_557_600;
+        this.earthAge = seconds / EARTH_SECONDS;
     }
 
-    double getSeconds() {
-        return seconds;
-    }
-
-    double onEarth() {
-        return earthAge;
-    }
-
-    double onMercury() {
-        return earthAge / Planet.MERCURY.getMultiplier();
-    }
-
-    double onVenus() {
-        return earthAge / Planet.VENUS.getMultiplier();
-    }
-
-    double onMars() {
-        return earthAge / Planet.MARS.getMultiplier();
-    }
-
-    double onJupiter() {
-        return earthAge / Planet.JUPITER.getMultiplier();
-    }
-
-    double onSaturn() {
-        return earthAge / Planet.SATURN.getMultiplier();
-    }
-
-    double onUranus() {
-        return earthAge / Planet.URANUS.getMultiplier();
-    }
-
-    double onNeptune() {
-        return earthAge / Planet.NEPTUNE.getMultiplier();
-    }
+    double getSeconds() { return seconds; }
+    double onEarth() { return earthAge; }
+    double onMercury() { return earthAge / Planet.MERCURY.getMultiplier(); }
+    double onVenus() { return earthAge / Planet.VENUS.getMultiplier(); }
+    double onMars() { return earthAge / Planet.MARS.getMultiplier(); }
+    double onJupiter() { return earthAge / Planet.JUPITER.getMultiplier(); }
+    double onSaturn() { return earthAge / Planet.SATURN.getMultiplier(); }
+    double onUranus() { return earthAge / Planet.URANUS.getMultiplier(); }
+    double onNeptune() { return earthAge / Planet.NEPTUNE.getMultiplier(); }
 
 }
