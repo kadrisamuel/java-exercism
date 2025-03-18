c14aed535127593febd1260c2d1890141f930f4f
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-18 10:52:24 +0100

    Initial solution

--- kindergarten-garden/src/main/java/KindergartenGarden.java
+++ kindergarten-garden/src/main/java/KindergartenGarden.java
@@ -1,13 +1,25 @@
 import java.util.List;
+import java.util.stream.IntStream;
 
 class KindergartenGarden {
+    private final static List<String> STUDENTS = List.of("Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"); 
+    private final List<List<Plant>> plants;
 
     KindergartenGarden(String garden) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String[] rows = garden.split("\n");
+
+        this.plants = IntStream.range(0, garden.length() / 4)
+            .mapToObj(student -> List.of(
+                Plant.getPlant(rows[0].charAt(student * 2)),
+                Plant.getPlant(rows[0].charAt(student * 2 + 1)),
+                Plant.getPlant(rows[1].charAt(student * 2)),
+                Plant.getPlant(rows[1].charAt(student * 2 + 1))
+            ))
+            .toList();
     }
 
     List<Plant> getPlantsOfStudent(String student) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return plants.get(STUDENTS.indexOf(student));
     }
 
 }
