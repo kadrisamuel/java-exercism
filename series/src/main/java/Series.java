b0511f558d2bf0c980e3e319347267b4902f40d9
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-21 14:01:17 +0100

    Initial solution

--- series/src/main/java/Series.java
+++ series/src/main/java/Series.java
@@ -1,11 +1,31 @@
 import java.util.List;
+import java.util.stream.IntStream;
 
 class Series {
-    Series(String string) {
-        throw new UnsupportedOperationException("Please implement the Series(string) constructor.");
+    final String string;
+
+    Series(String string) throws IllegalArgumentException {
+        if (string.isEmpty()) {
+            throw new IllegalArgumentException("series cannot be empty");
+        }
+        this.string = string;
     }
 
-    List<String> slices(int num) {
-        throw new UnsupportedOperationException("Please implement the Series.slices() method.");
+    List<String> slices(int num) throws IllegalArgumentException {
+        if (num < 1) {
+            throw new IllegalArgumentException("slice length cannot be negative or zero");
+        }
+        if (num > string.length()) {
+            throw new IllegalArgumentException("slice length cannot be greater than series length");
+        }
+
+        //int beginIndex = 0;
+        //int endIndex = num;
+        //int maxEndIndex = string.length();
+        return IntStream.rangeClosed(num, string.length())
+            .mapToObj(index -> string.substring(index - num, index))
+            .toList();
+
+        //return List.of("test");
     }
 }
