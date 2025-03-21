39623c5f1afd05203a715b01b8edee36c8e4d235
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-21 14:05:28 +0100

    Cleaned up version

--- series/src/main/java/Series.java
+++ series/src/main/java/Series.java
@@ -5,27 +5,20 @@ class Series {
     final String string;
 
     Series(String string) throws IllegalArgumentException {
-        if (string.isEmpty()) {
-            throw new IllegalArgumentException("series cannot be empty");
-        }
+        if (string.isEmpty()) { 
+            throw new IllegalArgumentException("series cannot be empty"); }
+            
         this.string = string;
     }
 
     List<String> slices(int num) throws IllegalArgumentException {
-        if (num < 1) {
-            throw new IllegalArgumentException("slice length cannot be negative or zero");
-        }
-        if (num > string.length()) {
-            throw new IllegalArgumentException("slice length cannot be greater than series length");
-        }
+        if (num < 1) { 
+            throw new IllegalArgumentException("slice length cannot be negative or zero"); }
+        if (num > string.length()) { 
+            throw new IllegalArgumentException("slice length cannot be greater than series length"); }
 
-        //int beginIndex = 0;
-        //int endIndex = num;
-        //int maxEndIndex = string.length();
         return IntStream.rangeClosed(num, string.length())
             .mapToObj(index -> string.substring(index - num, index))
             .toList();
-
-        //return List.of("test");
     }
 }
