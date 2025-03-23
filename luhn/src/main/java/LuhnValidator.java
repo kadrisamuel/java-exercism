ef47f58ae5bc985d1da098e30ec2191cdd9b6993
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-23 20:46:59 +0100

    Initial solution

--- luhn/src/main/java/LuhnValidator.java
+++ luhn/src/main/java/LuhnValidator.java
@@ -1,7 +1,40 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+
+
 class LuhnValidator {
 
     boolean isValid(String candidate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (candidate.strip().length() <= 1) {
+            return  false;
+        }        
+        if (Pattern.compile("[^0-9 ]").matcher(candidate).find()) {
+            return false;
+        }
+
+        // Make a list of ID numbers and reverse it
+        ArrayList<Integer> id = candidate.chars()
+            .filter(c -> Character.isDigit(c))
+            .mapToObj(c -> Character.getNumericValue(c))
+            .collect(Collectors.toCollection(ArrayList::new));
+        Collections.reverse(id);
+
+        // Double every second number on the list
+        IntStream.rangeClosed(0, id.size() / 2)
+            .forEach(i -> {
+                if (id.size() > i * 2 + 1) {
+                    int temp = 2 * id.get(i*2+1);
+                    id.set(i*2+1, (temp > 9)? temp - 9 : temp);
+                }
+            });
+
+        // Check if the sum is div by 10
+        return id.stream().reduce(0, (a, b) -> a + b) % 10 == 0;
     }
 
 }
