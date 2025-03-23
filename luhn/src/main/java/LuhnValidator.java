6cba519b101e45d373b602410a9d974e71a306d3
Kadri Samuel <kadrisamuel@gmail.com>
2025-03-23 21:51:51 +0100

    cpp inspired solution using only one loop and no streams

--- luhn/src/main/java/LuhnValidator.java
+++ luhn/src/main/java/LuhnValidator.java
@@ -1,37 +1,27 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 class LuhnValidator {
 
     boolean isValid(String candidate) {
         if (candidate.strip().length() <= 1) {
             return  false;
-        }        
-        if (Pattern.compile("[^0-9 ]").matcher(candidate).find()) {
-            return false;
+        }
+        int factor = 2, sum = 0;
+        for (int i = candidate.length() - 1; i > -1; i--) {
+            char c = candidate.charAt(i);
+            if (c == ' ') {
+                continue;
+            }
+            if (c < '0' || c > '9') {
+                return false;
+            }
+            factor = 3 - factor;
+            int digit = (c - '0') * factor;
+            if (digit > 9) {
+                digit -= 9;
+            }
+            sum += digit;
         }
 
-        // Make a list of ID numbers and reverse it
-        ArrayList<Integer> id = candidate.chars()
-            .filter(c -> Character.isDigit(c))
-            .mapToObj(c -> Character.getNumericValue(c))
-            .collect(Collectors.toCollection(ArrayList::new));
-        Collections.reverse(id);
-
-        // Double every second number on the list
-        IntStream.rangeClosed(0, id.size() / 2)
-            .forEach(i -> {
-                if (id.size() > i * 2 + 1) {
-                    int temp = 2 * id.get(i*2+1);
-                    id.set(i*2+1, (temp > 9)? temp - 9 : temp);
-                }
-            });
-
-        // Check if the sum is div by 10
-        return id.stream().reduce(0, (a, b) -> a + b) % 10 == 0;
+        return sum % 10 == 0;
     }
 
 }
