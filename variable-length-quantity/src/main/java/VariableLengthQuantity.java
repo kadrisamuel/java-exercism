46094c5a888379f688410b6aac678d9a5ed6d5fe
Kadri Samuel <kadrisamuel@gmail.com>
2025-04-09 10:06:26 +0200

    wrote idea for solution

--- variable-length-quantity/src/main/java/VariableLengthQuantity.java
+++ variable-length-quantity/src/main/java/VariableLengthQuantity.java
@@ -2,20 +2,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 class VariableLengthQuantity {
-    // limit is 4 bytes,
-    // bits go from N to 0,
-    // Encoding: 
-    // shift 8th bit (index 7) of every byte left to the next byte
-    // if current byte is rightmost (last), set 8th bit to 0
-    // otherwise set 8th bit to 1
-    // Decoding:
-    // 
-
+    /*
+     * limit is 4 bytes,
+     * bits go from N to 0,
+     * 
+     * Encoding: 
+     * shift 8th bit (index 7) of every byte left to the next byte
+     * if current byte is rightmost (last), set 8th bit to 0
+     * otherwise set 8th bit to 1
+     * 
+     * Decoding:
+     * long res = 0
+     * while byte is not last,
+     *  set 8th bit to 0 
+     *  and shift res left by 7
+     *  add byte to res
+     * if byte is last,
+     *  shift res left by 7
+     *  add  byte to res
+     *  add hex string of res to output list
+     * check if more bytes to process
+     */
+    
     // Returns 1 if the bit at position n is one
     // compress(x, 1L << n) == (x >> n & 1)
     final static int BITMASK = 0x80; // use | to set bit to 1, use &~ to set bit to 0
     
-    // TODO: error cases and decoding
+    // TODO: decoding
 
     List<String> encode(List<Long> numbers) {
         List<String> result = new ArrayList<>();
@@ -30,11 +43,14 @@ class VariableLengthQuantity {
 
             // getting the final byte and setting its 8th bit to 0
             // adding it to list
+            // shifting the bits to the right by 7
+            // so that the last bit of prev byte would be first of current
             currentNumber.add(String.format("%#2x", (byte)(num & ~BITMASK)));
             num >>= 7;
 
             // getting the other bytes and setting their 8th bits to 1
             // prepending them to the list
+            // shifting
             while (num != 0) {
                 currentNumber.add(0, String.format("%#2x", (byte)(num | BITMASK)));
                 num >>= 7;
@@ -50,6 +66,7 @@ class VariableLengthQuantity {
         if (bytes.get(bytes.size()-1) > 127) {
             throw new IllegalArgumentException("Invalid variable-length quantity encoding");
         }
+
         return null;
     }
 }
