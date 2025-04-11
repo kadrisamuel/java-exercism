4666d55ca9f8b1da330b6b276d85242061824b55
Kadri Samuel <kadrisamuel@gmail.com>
2025-04-11 16:41:57 +0200

    Initial solution

--- grade-school/src/main/java/School.java
+++ grade-school/src/main/java/School.java
@@ -1,17 +1,35 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.IntStream;
 
 class School {
+    // there are 7 grades
+    List<SortedSet<String>> roster = new ArrayList<>(7);
+    SortedSet<String> allStudents = new TreeSet<>();
+
+    public School() {
+        IntStream.range(0, 7)
+            .forEach(x -> roster.add(x, new TreeSet<>()));
+    }
 
     boolean add(String student, int grade) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (allStudents.add(student)) {
+            return roster.get(grade-1).add(student);
+        }
+        return false;
     }
 
     List<String> roster() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (allStudents.isEmpty()) {
+            return List.of();
+        }
+        return roster.stream().flatMap(s -> s.stream()).toList();
     }
 
     List<String> grade(int grade) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return roster.get(grade-1).stream().toList();
     }
 
 }
