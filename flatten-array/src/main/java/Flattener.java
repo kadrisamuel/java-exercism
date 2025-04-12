369c5806c597113aaa7a3d4267d3a72e3405970f
Kadri Samuel <kadrisamuel@gmail.com>
2025-04-12 21:40:57 +0200

    Small simplification

--- flatten-array/src/main/java/Flattener.java
+++ flatten-array/src/main/java/Flattener.java
@@ -3,25 +3,22 @@ import java.util.List;
 
 class Flattener {
 
+    @SuppressWarnings("unchecked")
     List<Object> flatten(List<?> list) {
-        List<Object> res = new ArrayList<>();
+        List<Object> flat = new ArrayList<>();
 
         for (Object elem : list) {
             if (elem == null) {
                 continue;
             }
             if (elem instanceof List) {
-                @SuppressWarnings("unchecked")
-                List<Object> temp = (List<Object>) elem;
-                if (!temp.isEmpty()) {
-                    res.addAll(flatten(temp));
-                }
+                flat.addAll(flatten((List<Object>)elem));
                 continue;
             }
-            res.add(elem);
+            flat.add(elem);
         }
         
-        return res;
+        return flat;
     }
 
 }
