e563c00a8eeed832b807843b11386745a37bfa0c
Kadri Samuel <kadrisamuel@gmail.com>
2025-04-12 21:36:55 +0200

    Initial solution

--- flatten-array/src/main/java/Flattener.java
+++ flatten-array/src/main/java/Flattener.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
 import java.util.List;
 
 class Flattener {
 
     List<Object> flatten(List<?> list) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<Object> res = new ArrayList<>();
+
+        for (Object elem : list) {
+            if (elem == null) {
+                continue;
+            }
+            if (elem instanceof List) {
+                @SuppressWarnings("unchecked")
+                List<Object> temp = (List<Object>) elem;
+                if (!temp.isEmpty()) {
+                    res.addAll(flatten(temp));
+                }
+                continue;
+            }
+            res.add(elem);
+        }
+        
+        return res;
     }
 
 }
