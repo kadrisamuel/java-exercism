b686bbc3fecf2f1d1b8385f9e7946e176021cd42
Kadri Samuel <kadrisamuel@gmail.com>
2025-04-13 16:52:02 +0200

    Had recursive solution, changed to intstream iterate, based on bobahops solution

--- collatz-conjecture/src/main/java/CollatzCalculator.java
+++ collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -1,19 +1,13 @@
+
+import java.util.stream.IntStream;
+
 class CollatzCalculator {
-    private int steps = 0;
 
     int computeStepCount(int start) {
         if (start < 1) {
             throw new IllegalArgumentException("Only positive integers are allowed");
         }
-        if (start == 1) {
-            return steps;
-        }
-        steps += 1;
-        if (start % 2 == 0) {
-            return computeStepCount(start / 2);
-        } else {
-            return computeStepCount(start * 3 + 1);
-        }
+        return (int) IntStream.iterate(start, num -> num != 1, num -> ((num & 1) == 1)? num * 3 + 1 : num >> 1).count();
     }
 
 }
