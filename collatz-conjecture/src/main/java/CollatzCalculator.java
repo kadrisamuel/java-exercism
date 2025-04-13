27340dafcddcb146b7f6702920952c64e1573bb4
Kadri Samuel <kadrisamuel@gmail.com>
2025-04-13 16:29:15 +0200

    Initial solution

--- collatz-conjecture/src/main/java/CollatzCalculator.java
+++ collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -1,7 +1,20 @@
 class CollatzCalculator {
+    private int steps = 0;
 
     int computeStepCount(int start) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (start < 1) {
+            throw new IllegalArgumentException("Only positive integers are allowed");
+        }
+        if (start == 1) {
+            return steps;
+        }
+        steps += 1;
+        if (start % 2 == 0) {
+            computeStepCount(start / 2);
+        } else {
+            computeStepCount(start * 3 + 1);
+        }
+        return steps;
     }
 
 }
