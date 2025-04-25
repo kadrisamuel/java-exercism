1af140c9dad7e2efe1042d3f61698a9f3c199422
Kadri Samuel <kadrisamuel@gmail.com>
2025-04-25 13:44:28 +0200

    Implemented basic case, first 3 tests pass

--- isbn-verifier/src/main/java/IsbnVerifier.java
+++ isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,7 +1,37 @@
+
+import java.util.List;
+import java.util.stream.IntStream;
+
 class IsbnVerifier {
+    private static final List<Character> ISBN_DIGITS = List.of('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'X');
 
     boolean isValid(String stringToVerify) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int[] isbnToVerify = strip(stringToVerify);
+
+        if (isbnToVerify.length != 10) {
+            return false;
+        }
+
+        int temp = IntStream.rangeClosed(1, 10)
+            .map(i -> isbnToVerify[10 - i] * i)
+            .sum();
+
+        boolean res = temp % 11 == 0;
+
+        return res;
+    }
+
+    private int[] strip(String stringToVerify) {
+        return stringToVerify.chars()
+            .mapToObj(nr -> (char) nr)
+            .filter(c -> ISBN_DIGITS.contains(c))
+            .mapToInt(c -> {
+                if (c == 'X') {
+                    return 10;
+                }
+                return c - '0';
+            })
+            .toArray();
     }
 
 }
