31d2c8ddc00442141535b3b5763a1f0eb824e2db
Kadri Samuel <kadrisamuel@gmail.com>
2025-05-02 15:32:52 +0200

    Separate method for printing for some of the scenarios, 10 tests pass

--- grep/src/main/java/GrepTool.java
+++ grep/src/main/java/GrepTool.java
@@ -8,21 +8,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class GrepTool {
-        /* Flags
-        * -n Prepend the line number and a colon (':') to each line in the output, placing the number after the filename (if present).
-        * -l Output only the names of the files that contain at least one matching line.
-        * -i Match using a case-insensitive comparison.
-        * -v Invert the program -- collect all lines that fail to match.
-        * -x Search only for lines where the search string matches the entire line.
-        */
-        boolean writeLineNumber = false;
-        boolean writeOnlyFilename = false;
-        boolean caseSensitive = true;
-        boolean invert = false;
-        boolean matchWholeLine = false;
+        private boolean writeLineNumber = false;
+        private boolean writeOnlyFilenames = false;
+        private boolean caseSensitive = true;
+        private boolean invert = false;
+        private boolean matchWholeLine = false;
 
-        List<Result> results;
-        Pattern pattern;
+        private boolean multipleFiles = false;
+
+        private List<Result> results;
+        private Pattern pattern;
 
 
     static class Result {
@@ -39,22 +34,56 @@ class GrepTool {
     }
 
     String grep(String phrase, List<String> flags, List<String> files) {
+        if (files.size() > 1) {
+            multipleFiles = true;
+        }
         parseFiles(phrase, files);
         setFlags(flags);
 
-        return results.get(0).line;
+        return printResult();
     }
 
+
+/*
+ * Return lines in the order in which they were found. 
+ * When searching in multiple files, each matching line 
+ * is prepended by the file name and a colon (':').
+ */
     private String printResult() {
-        
-        return results.get(0).line;
-    }
+        StringBuilder output = new StringBuilder();
 
+        results.stream().forEach(entry -> {
+            if (!output.isEmpty()) {
+                output.append("\n");
+            }
+            if (writeOnlyFilenames || multipleFiles) {
+                output.append(entry.filename);
+                if (!writeOnlyFilenames) {
+                    output.append(":");
+                }
+            }
+
+            if (writeLineNumber) {
+                output.append(entry.linenumber).append(":");
+            }
+            if (!writeOnlyFilenames) {
+                output.append(entry.line);
+            }
+        });
+
+        return output.toString();
+    }
+    /* Flags
+    * -n Prepend the line number and a colon (':') to each line in the output, placing the number after the filename (if present).
+    * -l Output only the names of the files that contain at least one matching line.
+    * -i Match using a case-insensitive comparison.
+    * -v Invert the program -- collect all lines that fail to match.        * -x Search only for lines where the search string matches the entire line.
+    */
     private void setFlags(List<String> flags) {
         flags.stream().forEach(flag -> {
             switch (flag) {
                 case "-n" -> writeLineNumber = true;
-                case "-l" -> writeOnlyFilename = true;
+                case "-l" -> {writeOnlyFilenames = true; writeLineNumber = false;}
                 case "-i" -> caseSensitive = false;
                 case "-v" -> invert = true;
                 case "-x" -> matchWholeLine = true;
@@ -63,7 +92,9 @@ class GrepTool {
             }
         });
     }
-
+    // TODO: handle case sensitivity flag
+    // TODO: handle inversion pattern
+    // TODO: handle whole line matching
     private void parseFiles(String phrase, List<String> files) {
         results = new ArrayList<>();
         pattern = Pattern.compile(phrase);
@@ -88,17 +119,3 @@ class GrepTool {
     }
 
 }
-
-/*
- * 
- * Return lines in the order in which they were found. 
- * When searching in multiple files, each matching line 
- * is prepended by the file name and a colon (':').
- * 
- * -n Prepend the line number and a colon (':') to each line in the output, placing the number after the filename (if present).
- * -l Output only the names of the files that contain at least one matching line.
- * -i Match using a case-insensitive comparison.
- * -v Invert the program -- collect all lines that fail to match.
- * -x Search only for lines where the search string matches the entire line.
- * 
- */
